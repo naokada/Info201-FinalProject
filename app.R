3764852be4c291aadb6b1d317555940b569dc156
Paul Yoo <yoosehy@uw.edu>
2018-03-05 22:57:31 -0800

    Improve User Interface

--- app.R
+++ app.R
@@ -7,20 +7,20 @@ ui <- dashboardPage(skin = "purple",
   dashboardHeader(title = "Place Lo-okup"),
   dashboardSidebar(
     sidebarMenu(
-      menuItem("Interact Map", tabName = "map", icon = icon("binoculars")),
-      menuItem("Data touch", tabName = "data", icon = icon("book"))
+      menuItem("Interact Map", tabName = "mapTab", icon = icon("binoculars")),
+      menuItem("Data touch", tabName = "dataTab", icon = icon("book"))
     )
   ),
   dashboardBody(
     tabItems(
       # First tab content
-      tabItem(tabName = "map",
+      tabItem(tabName = "mapTab",
               h2("Interactive Map"),
               leafletOutput("map")
       ),
       
       # Second tab content
-      tabItem(tabName = "data",
+      tabItem(tabName = "dataTab",
               fluidRow(
                 box(plotOutput("plot1", height = 250)),
                 
@@ -37,13 +37,31 @@ ui <- dashboardPage(skin = "purple",
 
 server <- function(input, output){
   
+  leafIcons <- icons(
+    iconUrl = "http://leafletjs.com/examples/custom-icons/leaf-green.png",
+    iconWidth = 38, iconHeight = 95,
+    iconAnchorX = 22, iconAnchorY = 94,
+    shadowUrl = "http://leafletjs.com/examples/custom-icons/leaf-shadow.png",
+    shadowWidth = 50, shadowHeight = 64,
+    shadowAnchorX = 4, shadowAnchorY = 62
+  )
+  
+  
   output$map <- renderLeaflet({
+    
     leaflet() %>%
-      addTiles() %>%  # Add default OpenStreetMap map tiles
-      addMarkers(lng=174.768, lat=-36.852, popup="The birthplace of R")
+      addTiles() %>% # Add default OpenStreetMap map tiles
+        setView(lng = -122.304010391235, lat = 47.6500093694438, zoom = 15) # UW lock
   })
   
-  
+  observeEvent(input$map_click, {
+    click <- input$map_click
+    text<-paste("Lattitude ", click$lat, "Longtitude ", click$lng)
+    
+    leafletProxy("map") %>% clearPopups() %>% clearMarkers() %>%
+      addMarkers(lng =  click$lng, lat =  click$lat, icon = leafIcons) %>%
+      addPopups(click$lng, click$lat + click$lat * 0.00005, text)
+  })
   
 }
 
